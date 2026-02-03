3b668a511b9a9d02fbf0ae7c3432c449774cab17
Neri Marschik <nerima@google.com>
2026-02-03 18:58:10 +0900

    Add fish shell config

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# fish_add_path $HOME/go/bin
+# fish_add_path $HOME/google-cloud-sdk/bin
+# fish_add_path (brew --prefix rustup)/bin
+# fish_add_path $HOME/.cargo/bin
+# fish_add_path $HOME/depot_tools
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# set -x LESS -r --use-color
+# set -x LS_COLORS (cat /Users/$(whoami)/.dotfiles/zsh/ls_colors_snazzy_8bit)
+
+# set -x LC_ALL en_US.UTF-8
+# set -x LC_CTYPE en_US.UTF-8
+
+source work.fish
